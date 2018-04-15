5bb7b2a186bb83e8733bace628fd87d34e05845e
Ji Yim <gsooyim@gmail.com>
2018-04-15 20:13:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(katz_deli)
   if katz_deli.size != 0
     katz_deli.each_with_index do |name, index|
-      puts "#{name} is in position #{index + 1}."
+      puts "The line is currently: #{index + 1}. #{name} "
     end
   else 
     puts "The line is currently empty."
