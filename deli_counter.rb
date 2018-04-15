37b977f60ac13f5f61209ed36607ff05c290ce47
Ji Yim <gsooyim@gmail.com>
2018-04-15 20:12:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  puts "The next person in line is #{katz_deli[0]}"
-  katz_deli[0].shift
+  if katz_deli.size != 0
+    puts "The next person in line is #{katz_deli[0]}"
+    katz_deli[0].unshift
+  else
+    puts "There is nobody waiting to be served!"
+  end
 end
