d37f85c8951c9f9c671dcaaf9c8479af4133b1c5
Ji Yim <gsooyim@gmail.com>
2018-04-15 20:11:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.size != 0
+    katz_deli.each_with_index do |name, index|
+      puts "#{name} is in position #{index + 1}."
+    end
+  else 
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "#{name} is in position #{katz.size}."
+end
+
+def now_serving(katz_deli)
+  puts "The next person in line is #{katz_deli[0]}"
+  katz_deli[0].shift
+end
